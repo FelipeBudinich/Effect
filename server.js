8d2766fc5b7c5c2dd382ea95e80b7cb6f7a97eba
Felipe Budinich <Felipe@Mac-mini-de-Rosario.local>
2024-06-09 00:10:24 -0400

    added save api endpoint

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
-import { glob } from 'glob';
 import path from 'path';
+import { glob } from 'glob';
+import { outputFile } from './tools/fs-utils.js';
 
 const app = express();
 const port = 3000;
@@ -28,6 +29,26 @@ app.get('/api/glob', async (req, res) => {
     } catch (err) {
         console.error('Error fetching files with glob:', err);
         res.status(500).send('Internal Server Error');
+    }
+});
+
+app.post('/api/save', async (req, res) => {
+    const { path: reqPath, data } = req.body;
+
+    if (!reqPath || !data) {
+        return res.send({ error: 1, msg: 'No Data or Path specified' });
+    }
+
+    if (!/\.js$/.test(reqPath)) {
+        return res.send({ error: 3, msg: 'File must have a .js suffix' });
+    }
+
+    try {
+        await outputFile(path.join(root, reqPath), data);
+        res.send({ error: 0 });
+    } catch (err) {
+        console.error('Error writing to file:', err);
+        res.send({ error: 2, msg: 'Couldn\'t write to file: ' + reqPath });
     }
 });
 
